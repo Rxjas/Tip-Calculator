d0f2615519c827bcc8c0d5025808f70083218073
rxjas <christopherrojas35@yahoo.com>
2022-02-07 09:19:14 -0700

    minimum working tip calc, only able to run using node

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+var bill = 80.71;
+var percent = 15;
+var people = 2;
+var tip = undefined;
+var answer = undefined;
+
+function calc(amount){
+     tip = amount * (percent / 100)
+    const endTip = tip.toFixed(2);
+    const endBill = (tip + bill).toFixed(2);
+    answer = (endBill / people).toFixed(2);
+    console.log(endTip);
+    console.log(endBill); 
+    console.log(answer); 
+
+}
+
+calc(bill)
