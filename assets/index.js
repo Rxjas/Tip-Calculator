c69bfe713d9e145818df2220426eeab1e180334e
rxjas <christopherrojas35@yahoo.com>
2022-02-08 21:55:07 -0700

    Able to draw values and console log them

--- assets/index.js
+++ assets/index.js
@@ -1,26 +1,28 @@
 //Getting all the IDs we want to work with
-console.log($('#bill'))
 var bill = document.getElementById('bill');
 var percent = document.getElementById('percent');
 var people = document.getElementById('people');
+var submitButton = document.getElementById('submit');
 var tipText = document.getElementById('tip');
 var totalText = document.getElementById('total');
 
-var bill2 = 77.71;
-var percent2 = 15;
-var people2 = 2;
-var tip2 = undefined;
-var answer2 = undefined;
+submitButton.addEventListener('click', () => console.log('submit button hit', bill.value, percent.value, people.value))
 
-function calc(amount){
-     tip = amount * (percent / 100)
-    const endTip = tip.toFixed(2);
-    const endBill = (tip + bill).toFixed(2);
-    answer = (endBill / people).toFixed(2);
-    console.log(endTip);
-    console.log(endBill); 
-    console.log(answer); 
+console.log(bill)
+// var bill2 = 77.71;
+// var percent2 = 15;
+// var people2 = 2;
+// var tip2 = undefined;
+// var answer2 = undefined;
 
-}
+// function calc(amount){
+//      tip = amount * (percent / 100)
+//     const endTip = tip.toFixed(2);
+//     const endBill = (tip + bill).toFixed(2);
+//     answer = (endBill / people).toFixed(2);
+//     console.log(endTip);
+//     console.log(endBill); 
+//     console.log(answer); 
+// }
 
-calc(bill)
+// calc(bill)
